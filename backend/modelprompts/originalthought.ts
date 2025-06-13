51738d54038cbbbeab0f2f1fc1ffa8e43d438cdd
Lawrence <131471451+HammyLA@users.noreply.github.com>
2025-06-12 17:59:57 -0700

    set up docker files and prisma schema

--- backend/modelprompts/originalthought.ts
+++ backend/modelprompts/originalthought.ts
@@ -17,5 +17,4 @@ const topics = [
 export default function originalThoughtPrompt (topic?: string) {
     const randomTopic = topics[Math.floor(Math.random() * topics.length)]
     return `You are a social media user making a general post about ${topic ? topic : randomTopic}. Be controversial and have the wrong opinion. Keep it a brief post. Don't use quotation marks unless it is a quote.`
-
 }
